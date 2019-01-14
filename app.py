840f72d8dcd20201139d34473492a6f97ac01547
MC3D <m.chapman.home@gmail.com>
2019-01-14 12:08:11 -0500

    adds requests

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+
+    response = requests.get('https://swapi.co/api/planets')
+    data = response.json()
+    planets = data['results']
+
+    # print(data['results'])
+
+    for planet in planets:
+        print(planet['name'])
 
     index_file = open('index.html', 'r')
     my_html = index_file.read()
